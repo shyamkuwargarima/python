2c53016d7e5ee41068e0a8bffd224980604656b2
shyamkuwargarima <shyamkuwargarima@gmail.com>
2026-03-10 08:22:36 +0530

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -21,4 +21,12 @@ def deposit(self, amount):
    print("Withdrawal amount must be positive.")
 
 def check_balance(self):
+
  print(f"Current Balance: ₹{self.balance}")
+
+account = BankAccount(101, 5000)
+
+account.check_balance()
+account.deposit(2000)
+account.withdraw(1500)
+account.check_balance()
